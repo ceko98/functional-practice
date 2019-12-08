796cc4def1352ab78f7c4a01f8f759fc32fb20f2
ceko98 <ceko9898@gmail.com>
2019-12-08 13:21:14 +0200

    Rework homework

--- 01/solutions/xno.rkt
+++ 01/solutions/xno.rkt
@@ -1,4 +1,5 @@
 #lang racket
+ (require racket/trace)
 
 (provide winner
          play)
@@ -21,7 +22,7 @@
 
 (define (winner b)
   (define (helper xss)
-    (cond 
+    (cond
       ((check-player "X" xss) "X")
       ((check-player "O" xss) "O")
       ((andmap (lambda (xs) (andmap id xs)) b) "D")
@@ -31,50 +32,51 @@
 ; "Dumb" "AI", plays the "next" free spot, going left-to-right, top-to-bottom.
 ; Put your own implementation here!
 
-(define (next-sign p) (if (equal? p "X") "O" "X"))
-
-(define (eval-board win me)
-  (cond
-    ((equal? win "D") 0)
-    (me 1)
-    ((not me) -1)))
+(define (next-p p) (if (equal? p "X") "O" "X"))
 
 (define (get-empty b)
   (define (helper i j)
-    (cond ((> i 2) '())
-          ((> j 2) (helper (+ i 1) 0))
-          ((not (matrix-ref b i j)) (cons (cons i j) (helper i (+ j 1))))
-          (else (helper i (+ j 1)))))
+    (cond 
+      ((> i 2) '())
+      ((> j 2) (helper (+ i 1) 0))
+      ((not (matrix-ref b i j)) (cons (cons i j) (helper i (+ j 1))))
+      (else (helper i (+ j 1)))))
   (helper 0 0))
 
-(define (helper-max b p me?)
-  (define (map-help)
-    (lambda (pr) (helper-min (place b (car pr) (cdr pr) p) (next-sign p) (not me?))))
+(define (eval-board b me)
+  (cond
+    ((equal? (winner b) "D") 0)
+    (me -1)
+    (else 1)))
+
+(define (helper-max b curr-p me)
+  (define (help)
+    (lambda (pr) (helper-min (place b (car pr) (cdr pr) curr-p) (next-p curr-p) (not me))))
 
   (if (winner b)
-    (eval-board (winner b) me?)
-    (foldr max -2 (map (map-help) (get-empty b)))))
+    (eval-board b me)
+    (foldr max -2 (map (help) (get-empty b)))))
 
-(define (helper-min b p me?)
-  (define (map-help)
-    (lambda (pr) (helper-max (place b (car pr) (cdr pr) p) (next-sign p) (not me?))))
+(define (helper-min b curr-p me)
+  (define (help)
+    (lambda (pr) (helper-max (place b (car pr) (cdr pr) curr-p) (next-p curr-p) (not me))))
 
   (if (winner b)
-    (eval-board (winner b) me?)
-    (foldr min 2 (map (map-help) (get-empty b)))))
+    (eval-board b me)
+    (foldr min 2 (map (help) (get-empty b)))))
 
 (define (play b p)
-  (define (foldr-help)
-    (lambda (pr curr)
-      (if (> (car pr) (car curr))
-        pr
-        curr)))
-
-  (cdr (foldr (foldr-help) (cons -2 (cons 0 0))
-    (zip-with
-      cons
-      (map
-        (lambda (pr)
-          (helper-max b p #t))
-        (get-empty b))
-      (get-empty b)))))
+  (define (help-lambda)
+    (lambda (acc val)
+      (if (> (cdr val) (cdr acc))
+        val
+        acc)))
+  (define (mapped-values)
+    (map 
+      (lambda (pr) (cons pr (helper-min (place b (car pr) (cdr pr) p) p #t)))
+      (get-empty b)))
+  (mapped-values))
+  ;(car (foldr (help-lambda) '((0 . 0) . -2) (mapped-values))))
+(play '(( #f #f)
+        (#f #f #f)
+        ("O" "X" "X")) "X")
