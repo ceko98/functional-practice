f31a14b02cfd6ce43a509b84bd52cc051bfbfda9
ceko98 <ceko9898@gmail.com>
2020-01-05 23:41:48 +0200

    Add Trees solutions

--- 03/src/Trees.hs
+++ 03/src/Trees.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE InstanceSigs #-}
-{-# OPTIONS_GHC -fplugin=HLint #-} -- run hlint on build via the hlint source plugin
+-- {-# OPTIONS_GHC -fplugin=HLint #-} -- run hlint on build via the hlint source plugin
 
 module Trees where
 
@@ -14,16 +14,29 @@ data Tree a
 
 instance Eq a => Eq (Tree a) where
   (==) :: Tree a -> Tree a -> Bool
-  (==) = undefined
+  (==) Empty Empty = True
+  (==) Empty _ = False
+  (==) _ Empty = False
+  (==) (Node x ltr1 rtr1) (Node y ltr2 rtr2) = x == y && ltr1 == ltr2 && rtr1 == rtr2
 
 insertOrdered :: Ord a => a -> Tree a -> Tree a
-insertOrdered = undefined
+insertOrdered x Empty = Node x Empty Empty
+insertOrdered x (Node a ltr rtr) = if x <= a
+  then Node a (insertOrdered x ltr) rtr
+  else Node a ltr (insertOrdered x rtr)
 
 listToBST :: Ord a => [a] -> Tree a
-listToBST = undefined
+listToBST = go Empty
+  where
+    go tr [] = tr
+    go tr (x:xs) = go (insertOrdered x tr) xs
 
 isBST :: Ord a => Tree a -> Bool
 isBST = undefined
+-- isBST Empty = True
+-- isBST (Node a ltr Empty) = treeVal ltr < a && isBST ltr
+-- isBST (Node a Empty rtr) = a < treeVal rtr && isBST rtr
+-- isBST (Node a ltr rtr) = treeVal ltr < a && a < treeVal rtr && isBST ltr && isBST rtr
 
 -- idea for implementing isBST - delete if you don't want it
 data BotTop a = Bot | Val a | Top
@@ -33,37 +46,43 @@ between :: Ord a => BotTop a -> BotTop a -> Tree a -> Bool
 between = undefined
 
 findBST :: Ord a => a -> Tree a -> Bool
-findBST = undefined
+findBST _ Empty = False
+findBST x (Node a ltr rtr) = x == a || findBST x ltr || findBST x rtr
 
 mapTree :: (a -> b) -> Tree a -> Tree b
-mapTree = undefined
+mapTree _ Empty = Empty
+mapTree f (Node a ltr rtr) = Node (f a) (mapTree f ltr) (mapTree f rtr)
 
 foldTree :: Monoid a => Tree a -> a
-foldTree = undefined
+foldTree Empty = mempty
+foldTree (Node a ltr rtr) = foldTree ltr <> a <> foldTree rtr
 
 foldMapTree :: Monoid b => (a -> b) -> Tree a -> b
-foldMapTree = undefined
+foldMapTree f = foldTree . mapTree f
 
 sumTree :: Num a => Tree a -> a
-sumTree = undefined
+sumTree = getSum . foldMapTree Sum
 
 allTree :: (a -> Bool) -> Tree a -> Bool
-allTree = undefined
+allTree f = getAll . foldMapTree (All . f)
 
 treeToList :: Tree a -> [a]
-treeToList = undefined
+treeToList = foldMapTree (:[])
 
 elemTree :: Eq a => a -> Tree a -> Bool
-elemTree = undefined
+elemTree x = getAny . foldMapTree (Any . (==) x)
 
 onMaybe :: (a -> Bool) -> a -> Maybe a
 onMaybe p x = if p x then Just x else Nothing
 
 findPred :: (a -> Bool) -> Tree a -> Maybe a
-findPred = undefined
+findPred f = getFirst . foldMapTree (First . onMaybe f)
 
 findAll :: (a -> Bool) -> Tree a -> [a]
-findAll = undefined
+findAll f = foldMapTree (help . onMaybe f)
+  where
+    help Nothing = []
+    help (Just a) = [a]
 
 ifJust :: Maybe a -> (a -> Maybe b) -> Maybe b
 ifJust Nothing _ = Nothing
@@ -78,7 +97,16 @@ data Direction
   deriving (Show, Eq)
 
 fetch :: [Direction] -> Tree a -> Maybe a
-fetch = undefined
+fetch [] (Node x _ _) = Just x
+fetch _ Empty = Nothing
+fetch (L:xs) (Node _ ltr _) = fetch xs ltr
+fetch (R:xs) (Node _ _ rtr) = fetch xs rtr
+
+mapDirections :: Tree a -> Tree (a, [Direction])
+mapDirections = go []
+  where
+    go _ Empty = Empty
+    go xs (Node x ltr rtr) = Node (x, xs) (go (L : xs) ltr) (go (R : xs) rtr)
 
 paths :: Tree a -> [(a, [Direction])]
-paths = undefined
+paths = treeToList . mapDirections
